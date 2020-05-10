7fcb0a259026401cd4d027494479b45046a3a4ca
unknown <you@example.comtob.moeller@gmx.de git config --global user.name Your>
2020-05-10 12:32:54 +0200

    added the display box for the recognized face

--- src/App.js
+++ src/App.js
@@ -42,7 +42,20 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
   }
 
   onInputChange = (event) => {
@@ -56,7 +69,7 @@ class App extends Component {
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input
       )
-      .then(response => this.calculateFaceLocation(response))
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
       .catch(err => console.log("we found an error: " + err))
   }
 
@@ -70,7 +83,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
